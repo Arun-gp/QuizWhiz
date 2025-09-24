470a4a6d164aba450f80ee154163839e6645950f
Arun <arunpandimca@gmail.com>
2025-09-24 06:42:38 +0000

    Build machine configuration: 2 cores, 8 GB Active Status: Failed to depl

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,20 +13,8 @@ const firebaseConfig = {
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
-const serverConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-};
-
-const config = typeof window === 'undefined' ? serverConfig : firebaseConfig;
-
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(config) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getDatabase(app);
 
